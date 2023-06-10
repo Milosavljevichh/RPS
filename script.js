b7435c78c5761aae8b8bbe712876c3e400d407f3
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2023-06-10 23:28:14 +0200

    Added win and defeat counters

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 
+let wins = 0
+let loses = 0
+
 function getPlayerChoice (value) {
     let cChoice = getComputerChoice();
     let playerChoice = value;
-    declareWinner(playerChoice, cChoice)
+    declareWinner(playerChoice, cChoice);
+    setCounter();
 }
 
 
@@ -27,36 +31,66 @@ function getComputerChoice () {
 
 }
 
+
+
+function setCounter () {
+    let wCounter = document.getElementById("wins")
+    let lCounter = document.getElementById("defeats")
+    lCounter.innerText = loses;
+    wCounter.innerText = wins;
+
+}
+
+
+
 function declareWinner (playerChoice, decision){
+    let elem = document.getElementById("result")
+
+    
     // Player chose Rock
     if (playerChoice === "Rock") {
 
             if (decision === "Rock") {
                 console.log("It's a tie!")
+                elem.innerText = "It's a tie!"
             } else if ( decision === "Paper") {
                 console.log("You lost!") 
+                elem.innerText = "You lost!"
+                loses++
             } else {
                 console.log("You've won!")
+                elem.innerText = "You've won!"     
+                wins++ 
             }
 
     //Paper choice
     } else if (playerChoice === "Paper") {
             if (decision === "Paper") {
                 console.log("It's a tie!")
+                elem.innerText = "It's a tie!"
             } else if ( decision === "Scissors") {
-                console.log("You lost!") 
+                console.log("You lost!")
+                elem.innerText ="You lost!"
+                loses++
             } else {
                 console.log("You've won!")
+                elem.innerText = "You've won!"
+                wins++ 
             }
-            
+
     //Scissors
     } else {
         if (decision === "Scissors") {
             console.log("It's a tie!")
+            elem.innerText = "It's a tie!"
         } else if ( decision === "Rock") {
-            console.log("You lost!") 
+            console.log("You lost!")
+            elem.innerText = "You lost!" 
+            loses++
         } else {
             console.log("You've won!")
+            elem.innerText = "You've won!"
+            wins++ 
         }
     }
 }
