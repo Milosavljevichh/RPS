9f78b02e0cf450d31dda82213ba0094bb687635b
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2023-06-13 00:10:15 +0200

    Added reset game system, modified interface, modified css, added README file

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a project made for "The odin project"
+
+Images used: <a href="https://www.flaticon.com/free-icons/rock" title="rock icons">Rock icons created by Freepik - Flaticon</a>
--- script.js
+++ script.js
@@ -3,31 +3,50 @@ let wins = 0
 let loses = 0
 
 
-function getPlayerChoice (value) {
-    let cChoice = getComputerChoice();
-    let playerChoice = value;
-    changeLeftImg(playerChoice)
-    declareWinner(playerChoice, cChoice);
-    setCounter();
-}
+function reset() {
+    let elem = document.getElementById("result")
 
-function changeLeftImg (choice) {
-
-    let leftImg = document.getElementById("left")
-
-    if (choice === "Rock") {
-        leftImg.src = "images/stone.png"
-    } else if (choice === "Paper") {
-        leftImg.src = "images/paper.png"
-    } else if (choice === "Scissors") {
-        leftImg.src = "images/scissors.png"
+    if (wins === 5) {
+    wins = 0;
+    loses = 0;
+    alert('You won!')
+    resetBorder()
+    elem.innerText = "Ready to play again?"
+} else if (loses === 5) {
+    alert('You lost!')
+    wins = 0;
+    loses = 0;
+    resetBorder()
+    elem.innerText = "Ready to play again?"
     }
 }
 
 
+function getPlayerChoice (value) {
+    resetBorder()
+    let cChoice = getComputerChoice();
+    let playerChoice = value;
+    declareWinner(playerChoice, cChoice);
+    setCounter();
+}
+
+function resetBorder() {
+    let rightImg = document.getElementById("right")
+    let leftImg = document.getElementById("left")
+    let middleImg = document.getElementById("middle")
+
+    if (rightImg.className || leftImg.className || middleImg.className === "glow-img") {
+        rightImg.classList.remove("glow-img")
+        leftImg.classList.remove("glow-img")
+        middleImg.classList.remove("glow-img")
+    }
+}
+
 function getComputerChoice () {
 
     let rightImg = document.getElementById("right")
+    let leftImg = document.getElementById("left")
+    let middleImg = document.getElementById("middle")
 
     let choice = Math.random()
     choice = Number(choice.toFixed(2))
@@ -36,15 +55,15 @@ function getComputerChoice () {
     
     if ( 2/3 <= choice && choice <= 1) {
         decision = "Rock"
-        rightImg.src = "images/stone.png"
+        leftImg.className = "glow-img"
         return decision
     } else if (1/3 <= choice && choice <= 2/3) {
         decision = "Paper"
-        rightImg.src = "images/paper.png"
+        middleImg.className = "glow-img"
         return decision
     } else {
         decision = "Scissors"
-        rightImg.src = "images/scissors.png"
+        rightImg.className = "glow-img"
         return decision
     }
 }
@@ -56,7 +75,6 @@ function setCounter () {
     let lCounter = document.getElementById("defeats")
     lCounter.innerText = loses;
     wCounter.innerText = wins;
-
 }
 
 
@@ -73,33 +91,40 @@ function declareWinner (playerChoice, decision){
             } else if ( decision === "Paper") {
                 elem.innerText = "You lost!"
                 loses++
+                reset()
             } else {
                 elem.innerText = "You've won!"     
                 wins++ 
+                reset()
             }
-
-    //Paper choice
-    } else if (playerChoice === "Paper") {
+            
+            //Paper choice
+        } else if (playerChoice === "Paper") {
             if (decision === "Paper") {
                 elem.innerText = "It's a tie!"
             } else if ( decision === "Scissors") {
                 elem.innerText ="You lost!"
                 loses++
+                reset()
             } else {
                 elem.innerText = "You've won!"
                 wins++ 
+                reset()
             }
-
-    //Scissors
-    } else {
-        if (decision === "Scissors") {
-            elem.innerText = "It's a tie!"
-        } else if ( decision === "Rock") {
-            elem.innerText = "You lost!" 
-            loses++
+            
+            //Scissors
         } else {
-            elem.innerText = "You've won!"
-            wins++ 
+            if (decision === "Scissors") {
+                elem.innerText = "It's a tie!"
+            } else if ( decision === "Rock") {
+                elem.innerText = "You lost!" 
+                loses++
+                reset()
+            } else {
+                elem.innerText = "You've won!"
+                wins++ 
+                reset()
         }
     }
 }
+
